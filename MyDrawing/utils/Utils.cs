c23a9baf854f086293e69e2017833ca66e7d2b33
Joseph home <joseph122792@gmail.com>
2024-12-02 22:36:16 +0800

    move GenerateRandomString to here

---
+++ MyDrawing/utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace MyDrawing.utils
+{
+    public class Utils
+    {
+        static public string GenerateRandomString()
+        {
+            Random random = new Random();
+            int length = random.Next(3, 11);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            StringBuilder result = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+                result.Append(chars[random.Next(chars.Length)]);
+
+            return result.ToString();
+        }
+    }
+}
